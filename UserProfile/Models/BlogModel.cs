89f88f891f9cb362b0c46c6508bb9c555afa23af
Navneet Kang <navneettech@yahoo.com>
2020-11-30 16:27:50 -0500

    BlogList

--- UserProfile/Models/BlogModel.cs
+++ UserProfile/Models/BlogModel.cs
@@ -8,11 +8,13 @@ namespace UserProfile.Models
     public class BlogModel
     {
         public int Id { get; set; }
+        public UserDetails Author { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
 
         public string Content { get; set; }
+        public ICollection<UserDetails> Blogs { get; set; }
 
-       
+
     }
 }
